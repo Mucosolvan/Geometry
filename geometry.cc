88b9ce7496a565b50a7986245ea56448346df51c
Marcin Wawerka <marcin.wawerka@gmail.com>
2016-11-09 22:10:51 +0100

    Add rectangle class

--- geometry.cc
+++ geometry.cc
@@ -1,6 +1,8 @@
+#include <utility>
 #include <algorithm>
+#include <assert.h>
 #include <iostream>
-
+// Pamietac o wykonywalnosci pliku ?
 class Vector {
 	
 	protected:
@@ -46,11 +48,86 @@ class Position : public Vector {
 		}
 };
 
+
+class Rectangle {
+	private:
+		int w;
+		int h;
+		Position position = Position(0,0);
+		
+	public:
+		Rectangle(const int width, const int height) {
+			assert (width > 0);
+			assert (height > 0);
+			w = width;
+			h = height;
+		}
+		
+		Rectangle(const int width, const int height, const Position p) {
+			assert (width > 0);
+			assert (height > 0);
+			w = width;
+			h = height;
+			position = p;
+		}
+		
+		int width() const {
+			return w;
+		}
+		
+		int height() const {
+			return h;
+		}
+		
+		Position pos() const {
+			return position;
+		}
+		
+		bool operator ==(const Rectangle &rect) const {
+			return (w == rect.width() && h == rect.height() &&
+					position == rect.pos());
+		}
+		
+		Rectangle& operator += (const Vector &vec) {
+			position += vec;
+			return *this;
+		} 
+		
+		int area() {
+			return w * h;
+		}
+		
+		// to modify after adding operator +
+		
+		Rectangle reflection() {
+			return Rectangle(w, h, Position(- position.x() - w, position.y()));
+		}
+		
+		std::pair<Rectangle, Rectangle> split_horizontally(int place) {
+			Rectangle first = Rectangle(w, place, position);
+			Rectangle second = Rectangle(w, h - place, 
+							   Position(position.x(), position.y() + place));
+			return std::make_pair(first, second);
+		}
+		
+		std::pair<Rectangle, Rectangle> split_vertically(int place) {
+			Rectangle first = Rectangle(place, h, position);
+			Rectangle second = Rectangle(w - place, h, 
+							   Position(position.x() + place, position.y()));
+			return std::make_pair(first, second);
+		}
+};
+
 int main() {
 	Position p = Position(100, 200);
-	std::cout << p.x();
+	//std::cout << p.x();
 	Vector v = Vector(200, 300);
-	std::cout << " " << ( (p += v) += v).y();
-	std::cout << " " << (Position (500 , 800) == p);
+	//std::cout << " " << ( (p += v) += v).y();
+	//std::cout << " " << (Position (500 , 800) == p);
+	
+	Rectangle r = Rectangle(2, 3, Position(1,0));
+	Rectangle r2 = Rectangle(2, 5, Position(1,0));
+	//Rectangle r = Rectangle();
+	std::cout << " " << (r == r2);
 	return 0;
 }
